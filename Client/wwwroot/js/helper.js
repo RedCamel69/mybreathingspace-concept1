ce95d3c23c940da188045c4e1d014ca0c7d7e60f
Richard Gilbert <richardjgilbert@hotmail.com>
2023-09-21 15:20:01 +0100

    bootstrap icons / setup

--- Client/wwwroot/js/helper.js
+++ Client/wwwroot/js/helper.js
@@ -22,13 +22,16 @@ export function ScrollToTop() {
 
 export function onDivScroll2(interval) {
     let selectHeader = document.getElementById('header');
+    let selectBackToTop = document.getElementById('back-to-top')';
     window.addEventListener('scroll', function (e) {       
        
         if (selectHeader) {
             if (window.scrollY > 100) {
-                selectHeader.classList.add('header-scrolled')
+                selectHeader.classList.add('header-scrolled');
+                selectBackToTop.classList.add('active');
             } else {
-                selectHeader.classList.remove('header-scrolled')
+                selectHeader.classList.remove('header-scrolled');
+                selectBackToTop.classList.remove('active');
             }
         }
     }, interval);
