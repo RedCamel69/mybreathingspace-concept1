459825c92580611248ba5da13bd83244c0a7e031
Richard Gilbert <richardjgilbert@hotmail.com>
2023-10-25 12:58:19 +0100

    wip

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazorBootstrap();
+//builder.Services.AddBlazorBootstrap();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
